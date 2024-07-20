436d46517a72ed4de3a192ccce87627effecb0d8
sayf-t <sayfti@gmail.com>
2024-07-20 20:20:58 +0800

    auth in server components

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,31 @@
+import { auth, signIn } from "@/auth";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import UserButton from "../UserButton/UserButton";
+
+export default async function NavBar() {
+  const session = await auth();
+  const user = session?.user;
+
+  return (
+    <header className="sticky top-0 bg-background px-3 shadow-sm">
+      <nav className="mx-auto flex h-14 w-full max-w-7xl items-center justify-between gap-3">
+        <Link href="/" className="font-bold">
+          Next-Auth v5 Tutorial
+        </Link>
+        { user ? <UserButton user={user}  /> : <SignInButton /> }
+      </nav>
+    </header>
+  );
+}
+
+function SignInButton() {
+  return (
+    <form action={async () => {
+      "use server";
+      await signIn();
+    }}>
+      <Button type="submit" >Sign In</Button>
+    </form>
+  );
+}
