3d3f03ae9478a2dc2bf86c6bfa1427af2687ad25
sayf-t <sayfti@gmail.com>
2024-07-20 20:42:55 +0800

    settings and redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import Image from "next/image";
+import prisma from "@/lib/prisma";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        Next Auth Demo - HOME
-      </div>
+    <main className="flex flex-col items-center gap-6 px-3 py-10">
+      <h1 className="text-center text-4xl font-bold">Next-Auth V5 Demo</h1>
+      <h2 className="text-center text-2xl font-semibold">Users</h2>
+      <ul className="list-inside list-disc">
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link href={`/user/${user.id}`} className="hover:underline">
+              {user.name || `User ${user.id}`}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
---
+++ app/settings/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import SettingsPage from "./SettingsPage";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Settings",
+};
+
+export default async function Page() {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/api/auth/signin?callbackUrl=/settings");
+  }
+  return <SettingsPage />;
+}
