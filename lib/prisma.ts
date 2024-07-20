be53a73a710bc6d888a34699bc693ec6181618a8
sayf-t <sayfti@gmail.com>
2024-07-20 19:39:22 +0800

    OAuth login

---
+++ lib/prisma.ts
@@ -0,0 +1,22 @@
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
+  const adapter = new PrismaNeon(neon);
+  return new PrismaClient({ adapter });
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+// This is a singleton pattern to ensure we only have one instance of Prisma
+// 🤡 Which just means, when we save changes in project, reuse previous prisma client
+//      multiple prisma clients bad,
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
