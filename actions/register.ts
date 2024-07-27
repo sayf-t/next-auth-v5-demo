014e429b8bb96b1cace1e577870b9f6f549d85e5
sayf-t <sayfti@gmail.com>
2024-07-27 10:58:16 +0800

    task: create user

--- actions/register.ts
+++ actions/register.ts
@@ -2,8 +2,10 @@
 
 import { revalidatePath, revalidateTag } from "next/cache";
 import * as z from "zod";
-
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -12,5 +14,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email Sent!"}
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already exists" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword, // important!
+    },
+  });
+
+  // TODO: send verification token email
+
+  return { success: "User created!"}
 };
