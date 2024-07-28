133a372c9a9e01e5caa6b7378c8eec00e0074a98
sayf-t <sayfti@gmail.com>
2024-07-28 12:13:56 +0800

    task: email verification wip

--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs";
 import { RegisterSchema } from "@/schemas";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -31,7 +32,9 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     },
   });
 
+  const verificationToken = await generateVerificationToken(email);
+
   // TODO: send verification token email
 
-  return { success: "User created!"}
+  return { success: "Confirmation email sent!" };
 };
